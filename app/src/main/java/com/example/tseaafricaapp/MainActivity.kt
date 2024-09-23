15f3ca6cbcab7cdce753a61a9400f95454a6e12e
Zuwarashe <zuwarashem1998@gmail.com>
2024-09-23 04:36:59 +0200

    zuwas first push-still developing

--- app/src/main/java/com/example/tseaafricaapp/MainActivity.kt
+++ app/src/main/java/com/example/tseaafricaapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.tseaafricaapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+       val NextBtn = findViewById<Button>(R.id.Next_Button)
+
+
+        NextBtn.setOnClickListener {
+            // Navigate to the Signup page
+            val intent = Intent(this, Settings::class.java)  // Make sure to replace with your actual signup activity class
+            startActivity(intent)
         }
+
     }
 }
